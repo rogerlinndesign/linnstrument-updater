5f99877f42d1384cdff0f124d7d0bac05672bdb8
Geert Bevin <gbevin@uwyn.com>
2014-10-25 00:56:20 +0200

    Renamed application target and code signed binary

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -30,7 +30,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "LinnStrumentUpdater";
+    const char* const  projectName    = "LinnStrument Updater";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
