479ed8a50065604e12fa533ec5c19f0a833c967d
Geert Bevin <gbevin@uwyn.com>
2023-10-11 08:06:24 -0400

    Included firmware 2.3.3 on macOS. Updated updater version to 2.0.12 build 13.

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -43,7 +43,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "LinnStrument Updater";
     const char* const  companyName    = "Roger Linn Design";
-    const char* const  versionString  = "2.0.11";
-    const int          versionNumber  = 0x2000b;
+    const char* const  versionString  = "2.0.12";
+    const int          versionNumber  = 0x2000c;
 }
 #endif
